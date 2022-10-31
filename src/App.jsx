95164faebdbff3c5babc041d4b6f464a08f2fff0
tecni2 <tecni2.elie.elie@gmail.com>
2022-10-30 20:26:08 -0500

    Added icons

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,17 @@ function App() {
   const [isCentigrade, setIsCentigrade] = useState(true);
   const changeValue = () => {
     // alert("falta agregar funcionalidad")
-    setIsCentigrade(!isCentigrade);
-    console.log("Cambie a", isCentigrade);
-  };
+    setIsCentigrade(!isCentigrade)
+    console.log('Cambie a', isCentigrade);
+  }
 
   useEffect(() => {
     const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
-    };
-
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+   };
+   
     const success = (pos) => {
       // const crd = pos.coords;
 
@@ -32,50 +32,39 @@ function App() {
         .then((res) => setWeather(res.data))
         .catch((err) => console.log("fallo"));
     };
-
+  
     navigator.geolocation.getCurrentPosition(success, Option);
   }, []);
   // setTimeout(() => {
   // }, 1000)
-  console.log("info de la API", weather);
+  console.log('info de la API', weather);
 
   return (
     <div className="App">
       <h1>Weather app</h1>
-      <h3>
-        <img className="icon" src="../src/assets/location_on-.svg" alt="" />
-        {weather.sys?.country} {weather.name}
-      </h3>
+      <h3><img className="icon" src="../src/assets/location_on-.svg" alt="" />
+      {weather.sys?.country} {weather.name}</h3>
       <div className="card">
         <div className="img-weather">
-          <img
-            className="img-img-weather"
-            src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`}
-            alt=""
-          />
-          <p>Temperatura</p>
-          <p>
-            {isCentigrade
-              ? Math.round(weather.main?.temp - 273.15) + "°C"
-              : Math.round(((weather.main?.temp - 273.15) * 9) / 5 + 32) + "°F"}
-          </p>
+      <img className="img-img-weather" src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
+      <p>Temperatura</p>
+      <p>
+        {isCentigrade ? Math.round(weather.main?.temp-273.15) + "°C" : Math.round((weather.main?.temp-273.15)*9/5+32) + "°F"} 
+      </p>
         </div>
         <div className="description-weather">
-          <p>"{weather.weather?.[0].description}"</p>
-          <p>
-            <img className="icon-wind" src="../src/assets/wind.svg" alt="" />
-            Wind Speed: {weather.wind?.speed}
-          </p>
-          <p>Clouds: {weather.clouds?.all}%</p>
-          <p>
-            <img className="icon-wind" src="../src/assets/pressue.svg" alt="" />
-            Pressure: {weather.main?.pressure} mb
-          </p>
-        </div>
+      <p>"{weather.weather?.[0].description}"</p>
+      <p><img className="icon-wind" src="../src/assets/wind.svg" alt="" /> 
+        Wind Speed: {weather.wind?.speed}</p>
+      <p>Clouds: {weather.clouds?.all}%</p>
+      <p><img className="icon-wind" src="../src/assets/elea.jpg" alt="" />
+       Pressure: {weather.main?.pressure} mb</p>
+     </div>
+        
       </div>
-      <button onClick={changeValue}>
-        {isCentigrade ? "Change to °F" : "Change to °C"}
-      </button>
+        <button onClick={changeValue}>
+          {isCentigrade ? "Change to °F" : "Change to °C"}
+        </button>
     </div>
   );
 }
